5eab14fb0e3cf76669d1bf33d6cee7fd208a4489
it21505294 <87332117+it21505294@users.noreply.github.com>
2021-08-18 22:07:46 +0530

    Update Exercise02.cpp

--- Exercise02.cpp
+++ Exercise02.cpp
@@ -6,7 +6,7 @@ int main() {
   
    // 1. Create a Box type object called box1
   
-  
+    Box box1;
    // ======= DO NOT CHANGE THE INPUT ===========
    int height, length, width;
    cout << "Enter the Height of the Box : ";
@@ -19,6 +19,9 @@ int main() {
   
    // 2. Use setters assign height, length, width throw
   
+   box1.setLength(length);
+   box1.setWidth(width);
+   box1.setHeight(height);
    // === DO NOT CHANGE THE OUTPUT =============
    cout << "Box Height " << box1.getHeight() << endl;
    cout << "Box Length " << box1.getLength() << endl;
@@ -27,4 +30,8 @@ int main() {
    // ==========================================
       return 0;
 }
+
+
+
+
 
