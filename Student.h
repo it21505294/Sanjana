dcba21bb82369be12e03410eae8537fbd00980c3
it21505294 <87332117+it21505294@users.noreply.github.com>
2021-08-18 22:08:48 +0530

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,9 +1,9 @@
 class Student {
-  // private section
-  //    int studentId
-  //    name <- 20 charcters
+  private:
+    int studentId;
+    char name[20];
   
-  // public section
-  //    assignDetails() method declaration
-  //    display() method declaration
+  public:
+    void assignDetails(int stdId, char sname[]);
+    void display();
 };
